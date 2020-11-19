4ee39a84b92441e5d8b8b9da7e45c063ad82f96c
Piyush <piyush.dwivedi@infogain.com>
2020-11-19 22:17:39 +0530

    Project Initiated

---
+++ PopularArticle/PopularArticle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PopularArticle
+//
+//  Created by Piyush on 19/11/20.
+//  Copyright © 2020 Xebia Tech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
