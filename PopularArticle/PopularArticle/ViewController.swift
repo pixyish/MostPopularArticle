1331175d3809507d2e8dddaa331b33aacd89d486
Piyush <piyush.dwivedi@infogain.com>
2020-11-20 12:42:33 +0530

    Cell UI and logic integrated

--- PopularArticle/PopularArticle/ViewController.swift
+++ PopularArticle/PopularArticle/ViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 class ViewController: UIViewController {
 
     var results:[Results]?
+    @IBOutlet weak private var tblView:UITableView!
+    private var reuseIdentifier = "articleCell"
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -19,7 +21,13 @@ class ViewController: UIViewController {
 
     //MARK:- Private Methods
     func setUI() {
+        self.registerXib()
         self.callApi(path: PAConstant.getArticleURl(path: .path7))
+    }
+    
+    func registerXib() {
+        let cellNib = UINib(nibName: "PAArticleTableViewCell", bundle: nil)
+        self.tblView.register(cellNib, forCellReuseIdentifier: reuseIdentifier)
     }
     
     func parseResponse(_ data: Data) -> JSONResult? {
@@ -27,9 +35,6 @@ class ViewController: UIViewController {
             let decoder = JSONDecoder()
             let responseModel = try? decoder.decode(JSONResult.self, from: data)
             return responseModel
-        } catch {
-            print("Data parsing error: \(error.localizedDescription)")
-            return nil
         }
     }
     
@@ -39,7 +44,8 @@ class ViewController: UIViewController {
                 if let jsonResult = self?.parseResponse(response as! Data) {
                     if let status = jsonResult.status,status.uppercased().contains("OK"), let dataResult = jsonResult.results {
                             self?.results = dataResult
-                            print(self?.results?.count)
+                            self?.tblView.reloadData()
+                            print(self?.results?.count ?? 0)
                      }
                    }
             } else {
@@ -49,5 +55,25 @@ class ViewController: UIViewController {
             }
         }
     }
+}
+
+extension ViewController:UITableViewDataSource,UITableViewDelegate {
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.results?.count ?? 0
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let articleCell = tblView.dequeueReusableCell(withIdentifier: reuseIdentifier) as? PAArticleTableViewCell {
+            articleCell.resultInfo = self.results?[indexPath.row]
+            return articleCell
+        }
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "defaultCell")
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 150
+    }
+        
 }
 
--- PopularArticle/Views/PAArticleTableViewCell.swift
+++ PopularArticle/Views/PAArticleTableViewCell.swift
@@ -10,15 +10,28 @@ import UIKit
 
 class PAArticleTableViewCell: UITableViewCell {
 
-    @IBOutlet weak private var imgView: UIImageView!
+    @IBOutlet weak private var imgView: UIImageView! {
+        didSet {
+            self.imgView.layer.cornerRadius = self.imgView.frame.size.width/2
+        }
+    }
     @IBOutlet weak private var titleLbl: UILabel!
     @IBOutlet weak private var descriptionLbl: UILabel!
-    @IBOutlet weak private var dateLbl: UIButton!
-    
+    @IBOutlet weak private var sectionLbl: UILabel!
+    @IBOutlet weak private var dateBtn: UIButton!
+    var resultInfo:Results? {
+        didSet {
+            if let info = resultInfo {
+                titleLbl.text       = info.title
+                descriptionLbl.text = info.abstract
+                sectionLbl.text     = info.section
+                dateBtn.setTitle(info.published_date, for: UIControl.State.normal)
+            }
+        }
+    }
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-    
     
 }
